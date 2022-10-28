4558d8d5da9eeea65c0c53a935a9899ee028f0c7
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-10-28 17:46:15 +0300

    upluding

--- capsules.js
+++ capsules.js
@@ -3,7 +3,7 @@ const fetchData = async (url) => {
     try {  
         const res = await fetch(url);
         const data = await res.json();
-        console.log(data);
+        //console.log(data);
         return data;
     } catch (error) {
         console.log(error);
@@ -14,27 +14,36 @@ const fetchData = async (url) => {
 //fetchData(`https://capsules7.herokuapp.com/api/group/one`);// class with 11
 //fetchData(`https://capsules7.herokuapp.com/api/group/two`); // class with 14
 
+
 async function getData() {
     let dataFileOne = await (fetchData('https://capsules7.herokuapp.com/api/group/one'));
     let dataFileTwo = await (fetchData('https://capsules7.herokuapp.com/api/group/two'));
-
     const meragdData = dataFileOne.concat(dataFileTwo);
-    console.log(meragdData);
+   // console.log(meragdData);
+
     const people = [];
     for (let index = 0; index < meragdData.length; index++) {
         const person = fetchData(`https://capsules7.herokuapp.com/api/user/${meragdData[index].id}`);
         people.push(person);
        
     }
+    // log data
     const data = await Promise.all(people)
-    console.log(data);
-}
+    //console.log(data);
+    structurChar(meragdData);
+};
 getData();
 
 
-// log data
-
-
 // display the data
+
+const structurChar = (arr, arr1) => {
+    return arr.map((char) => {
+        const newChar = {id: char.id, firstName: char.firstName, lastName: char.firstName};
+        console.log(newChar);
+    });
+
+};
+
 
 // style html
